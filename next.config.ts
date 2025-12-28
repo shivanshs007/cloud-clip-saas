9e4d17b6e25e2735a2d8d3186836b500302f0fe8
shivanshs007 <shivanshs864@gmail.com>
2025-12-29 00:03:39 +0530

    fixed video upload issue

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  experimental: {
-    // e.g. allow up to 100MB
-    proxyClientMaxBodySize: '100mb',
-  },
 };
 
 export default nextConfig;
